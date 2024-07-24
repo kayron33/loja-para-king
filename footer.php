a317a521f42a50bf7abc55acf9e526e5ffeff6d4
kayron33 <129231712+kayron33@users.noreply.github.com>
2024-07-24 15:09:54 -0300

    nova atualização

---
+++ footer.php
@@ -0,0 +1,54 @@
+<!--INICIO DO FOOTER  DA LOJA -->
+<footer class="rodape">
+<div class="container">
+    <div class="linha">
+        <div class="rodape-col-1">
+            <h3>Baixe o nosso app</h3>
+            <p>Baixe o nosso aplicativo nas melhores plataformas</p>
+            <div class="app-logo">
+                <img src="asset/img/google.png" alt="">
+                <img src="asset/img/apple.png" alt="">
+            </div>
+        </div>
+
+        <div class="rodape-col-2">
+            <img src="asset/img/logo-2.png" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+        </div>
+
+        <div class="rodape-col-3">
+            <h3>Mais informações</h3>
+            <ul>
+                <li>Cupons</li>
+                <li>blog</li>
+                <li>politica</li>
+                <li>privacidade</li>
+            </ul>
+        </div>
+
+        <div class="rodape-col-4">
+            <h3>Redes Sociais</h3>
+            <ul>
+                <li>Facebook</li>
+                <li>Instagram</li>
+                <li>YouTube</li>
+                <li>Twitter</li>
+            </ul>
+        </div>
+    </div>
+    <hr>
+    <p class="direitos">
+        &#169; Todos os direitos reservados a KING sites 
+    </p>
+</div>
+</footer>
+<!--FIM DO FOOTER  DA LOJA -->
+
+
+
+
+<script  type = "módulo"  src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js" > </script>
+<script  nomodule  src = "https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js" > </script>
+    <script src="asset/js/loja.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <title>Loja</title>
+</head>
+<body>
+<!--INICIO DO BANNER DA LOJA -->
+<div class="novoMenu">
+    <!--INICIO DO CONTAINER DA LOJA -->
+    <div class="container">
+        <!--INICIO DA NAVEGAÇÃO DA LOJA -->
+        <div class="navbar">
+            <div class="logo">
+                <a href="index.html"><img src="asset/img/logoEscura.png" alt="logo da loja" width="125px"></a>
+            </div>
+            <!--INICIO DO MENU DE NAVEGAÇÃO DA LOJA -->
+            <nav >
+                <ul id="menuItens">
+                    <li><a href="index.php" title="">Inicio</a></li>
+                    <li><a href="produtos.php" title="">Produtos</a></li>
+                    <li><a href="empresa.php" title="">Empresa</a></li>
+                    <li><a href="contatos.php" title="">Contatos</a></li>
+                    <li><a href="minha-conta.php" title="">Minha Conta</a></li>
+                </ul>
+            </nav>
+            <!--FIM DO MENU DE NAVEGAÇÃO DA LOJA -->
+
+
+            <a href="carrinho.php"><img src="asset/img/carrinho2.png" alt="" width="30px" height="30px"></a>
+            <img src="asset/img/menu.png" alt="" class="menu-celular" onclick="menucelular()">
+
+        </div>
+        <!--FIM DA NAVEGAÇÃO DA LOJA -->
